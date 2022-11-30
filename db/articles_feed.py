c396a836cd196f6457b22179c50b0fe06eaff193
andy <a5706617@gmail.com>
2022-11-30 23:52:16 +0800

    commit

--- db/articles_feed.py
+++ db/articles_feed.py
@@ -17,7 +17,7 @@ articles = [
                             "remaining essentially unchanged. It was popularised in the 1960s with the release of "
                             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
                             "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "image": "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
     },
     {
         "title": "Article2",
@@ -37,7 +37,7 @@ articles = [
                             "remaining essentially unchanged. It was popularised in the 1960s with the release of "
                             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
                             "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/7026467/pexels-photo-7026467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "image": "https://plus.unsplash.com/premium_photo-1668877838433-f32286997be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
     },
     {
         "title": "Article3",
@@ -57,7 +57,7 @@ articles = [
                             "remaining essentially unchanged. It was popularised in the 1960s with the release of "
                             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
                             "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://images.pexels.com/photos/8110956/pexels-photo-8110956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "image": "https://images.unsplash.com/photo-1669554108285-dc5c2786ed61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80"
     },
     {
         "title": "Article4",
@@ -77,6 +77,6 @@ articles = [
                             "remaining essentially unchanged. It was popularised in the 1960s with the release of "
                             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
                             "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "image": "https://www.pexels.com/zh-tw/photo/cgi-12843338/"
+        "image": "https://images.unsplash.com/photo-1661961110144-12ac85918e40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
     }
 ]
