74283f62a64dd28a5e7abb1d60c4d037247a5ff7
andy <a5706617@gmail.com>
2022-12-12 16:14:50 +0800

    commit

--- db/users_feed.py
+++ db/users_feed.py
@@ -1,27 +1,27 @@
 users = [
     {
-        "username": "Irene",
-        "avatar":"https://images.pexels.com/photos/12942114/pexels-photo-12942114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "email":"I1231@gamil.com"
+        "username": "GD",
+        "avatar": "https://images.unsplash.com/photo-1670758548386-5ce5c355b5e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        "email": "I1231@gamil.com"
     },
     {
-        "username": "Seulgi",
-        "avatar": "https://images.pexels.com/photos/11098737/pexels-photo-11098737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "username": "Taeyang",
+        "avatar": "https://images.unsplash.com/photo-1667786119855-92c31bc7b75b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
         "email": "S1231@gamil.com"
     },
     {
-        "username": "Wendy",
-        "avatar": "https://images.pexels.com/photos/11139223/pexels-photo-11139223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "username": "Top",
+        "avatar": "https://images.unsplash.com/photo-1667501928453-ee6694d6450d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
         "email": "W1231@gamil.com"
     },
     {
-        "username": "Joy",
-        "avatar": "https://images.pexels.com/photos/14040258/pexels-photo-14040258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "username": "Daesung",
+        "avatar": "https://images.unsplash.com/photo-1668939072587-fc9fc2988f6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "email": "J1231@gamil.com"
     },
     {
-        "username": "Yeri",
-        "avatar": "https://images.pexels.com/photos/12495840/pexels-photo-12495840.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "username": "Serui",
+        "avatar": "https://images.unsplash.com/photo-1633190323610-49a306581216?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         "email": "Y1231@gamil.com"
     },
 ]
